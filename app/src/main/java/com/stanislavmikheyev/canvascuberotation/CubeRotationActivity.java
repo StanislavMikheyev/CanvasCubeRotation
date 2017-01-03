652ff42fd1032d2207d6f1b6e10574b43d63e215
StanislavMikheyev <stanislav.mikheyev@gmail.com>
2017-01-03 08:03:38 +0100

    Add CubeRotationActivity

---
+++ app/src/main/java/com/stanislavmikheyev/canvascuberotation/CubeRotationActivity.java
@@ -0,0 +1,13 @@
+package com.stanislavmikheyev.canvascuberotation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CubeRotationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cube_rotation);
+    }
+}
