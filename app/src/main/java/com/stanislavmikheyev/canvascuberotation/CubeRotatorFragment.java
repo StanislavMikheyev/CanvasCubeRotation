16d1effb2f76679d14e372c73088613d327be837
StanislavMikheyev <stanislav.mikheyev@gmail.com>
2017-01-13 21:55:32 +0100

    Change API level and start work on Fragment

---
+++ app/src/main/java/com/stanislavmikheyev/canvascuberotation/CubeRotatorFragment.java
@@ -0,0 +1,15 @@
+package com.stanislavmikheyev.canvascuberotation;
+
+import android.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by stani on 1/13/2017.
+ */
+
+public class CubeRotatorFragment extends Fragment implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
